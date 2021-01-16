31e742992c915b7c750c9e58991b9eb70281cc00
kostispodaras <kostis.podaras@agileactors.com>
2021-01-16 16:19:07 +0200

    test

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,34 +4,23 @@ import { compose } from 'core/utils';
 import ProtectedRoute from 'core/components/protected-route';
 import withModelActivation from 'core/components/withModelActivation';
 import { Router } from 'core/components/router';
-import { withModelProps } from 'core/components/model-props';
-
-import NotFound from 'components/not-found';
-import { authenticated } from 'models/user';
 
 import { reducer } from './ui-model';
 import { Layout } from './components';
 import { Home } from './routes';
 
-type Props = {
-  authenticated: boolean,
-};
-
-const App = ({ authenticated }: Props) => (
+const App = () => (
   <Layout>
     <Router basepath="/app/">
-      <ProtectedRoute path="/" authenticated={authenticated} component={Home} />
-      <NotFound default authenticated={authenticated} />
+      <ProtectedRoute path="/" component={Home} />
     </Router>
   </Layout>
 );
 
-export { App };
 export default compose(
   withModelActivation({
     reducer: {
       ui: reducer,
     },
   }),
-  withModelProps({ authenticated }),
 )(App);
